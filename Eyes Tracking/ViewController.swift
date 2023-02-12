eb292bd0b5b4f64bd5c4b80a1b92b4279bb722b5
Dilara Karakaş <60318526+ddilarakarakas@users.noreply.github.com>
2023-02-12 22:22:37 +0300

    Update ViewController.swift

--- Eyes Tracking/ViewController.swift
+++ Eyes Tracking/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  Eyes Tracking
-//
-//  Created by Virakri Jinangkul on 6/6/18.
-//  Copyright © 2018 virakri. All rights reserved.
-//
 
 import UIKit
 import SceneKit
